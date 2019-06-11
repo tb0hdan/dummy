7c446f572ff05a320b0ca8f65de112ded146a243
akhripko <alexey.hripko@gmail.com>
2019-06-11 19:04:40 +0300

    [initial] add services - step 2

--- service/service.go
+++ service/service.go
@@ -38,19 +38,19 @@ func (s *service) Run(ctx context.Context, wg *sync.WaitGroup) {
 	wg.Add(1)
 
 	go func() {
+		defer wg.Done()
+		err := s.http.ListenAndServe()
+		if err != nil {
+			log.Error(err)
+		}
+	}()
+
+	go func() {
 		<-ctx.Done()
 		sdCtx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 		err := s.http.Shutdown(sdCtx)
 		if err != nil {
 			log.Error("service shutdown error:", err)
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		err := s.http.ListenAndServe()
-		if err != nil {
-			log.Error(err)
 		}
 	}()
 }
